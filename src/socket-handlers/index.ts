6672e80c610c65d844a8d7e953d9b1363ac3c180
revandobachtiar <revandobachtiar49@gmail.com>
2026-07-09 11:42:21 +0700

    09072026-repair index and tsconfig

--- src/socket-handlers/index.ts
+++ src/socket-handlers/index.ts
@@ -2,12 +2,13 @@
 import robotHandlers from "./identity.ts"
 import incidentHandlers from "./fall-detection.ts"
 import speechModuleListener from "./led-module.ts"
-import robotModeHandlers from "./robotMode"
-import alarmHandlers from "./alarm"
+import robotModeHandlers from "./robotMode.ts"
+import alarmHandlers from "./alarm.ts"
 import languageSetupHandlers from "./language-setup.ts"
-import wifiModuleHandler from "./wifi-setup"
+import wifiModuleHandler from "./wifi-setup.ts"
 import volumeModuleHandler from "./volume.ts" 
-import brightnessModuleHandler from "./brightness"
+import brightnessModuleHandler from "./brightness.ts"
+import voiceCommandHandler from "./voice-command.ts"
 
 export default function registerHandlers(socket : any,io: any){
     robotHandlers(socket,io);
@@ -17,7 +18,8 @@ export default function registerHandlers(socket : any,io: any){
     alarmHandlers(socket,io)
     languageSetupHandlers(socket,io);
     alarmHandlers(socket,io);
-    wifiModuleHandler(socket,io)
-    volumeModuleHandler(socket,io)
-    brightnessModuleHandler(socket,io)
+    wifiModuleHandler(socket,io);
+    volumeModuleHandler(socket,io);
+    brightnessModuleHandler(socket,io);
+    voiceCommandHandler(socket,io);
 }               
