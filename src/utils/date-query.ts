8569a64dda47bb70cf587437a21296b65fbb47c6
hariachmad <hari08achmad@gmail.com>
2026-07-12 21:10:49 +0700

    modify date query

--- src/utils/date-query.ts
+++ src/utils/date-query.ts
@@ -1,14 +1,16 @@
 import { z } from "zod";
 
-
 export const dateQuery = z.object({
     year: z.number().int().min(1970).max(2100),
     month: z.number().int().min(1).max(12),
-    day: z.number().int().min(1).max(31),
+    week: z.number().int().min(1).max(5).optional(),
+    day: z.number().int().min(1).max(31).optional(),
     hour: z.number().int().min(0).max(23).optional(),
     minute: z.number().int().min(0).max(59).optional(),
     second: z.number().int().min(0).max(59).optional(),
 }).refine((val) => {
+    if (val.day === undefined) return true;
+
     const date = new Date(
         val.year,
         val.month - 1,
